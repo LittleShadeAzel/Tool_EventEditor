b07afc2300ec22ee692203ca3ce9b86bd3e3700e
LittleShadeAzel <alexander.cvetanovic@bluewin.ch>
2019-11-05 18:27:18 +0100

    Updated Documentaion.

---
+++ Source/EventEditor/Assets/EventEditor/Scripts/GameEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LGP.EE {
+    /// <summary>
+    /// Game Event Class. This monobehaviour enables game objects to have an event editor.
+    /// </summary>
+    public class GameEvent : MonoBehaviour {
+        #region Variables
+        #endregion
+
+        #region Unity Methods
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
